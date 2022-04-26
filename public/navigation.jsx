4b1d37d9b3f92553d818004a6eac1e56b7191a98
Destin-Niyomufasha <92045421+Destin-Niyomufasha@users.noreply.github.com>
2022-04-26 14:55:30 +0200

    Initial commit

---
+++ public/navigation.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import "./navigation.css" 
+
+
+const navigation = () =>{
+    return(
+      <section>
+      
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#support">Support</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#blog">Blog</a></li>
+          <li><button>Portifolios</button></li>
+        </ul>
+      </section>
+    )
+}
+
+export default navigation
